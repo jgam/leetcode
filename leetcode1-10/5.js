28a197582cac675458f47be12c13995b165b243f
jgam <jgam@alumni.nd.edu>
2019-06-20 10:50:00 +0900

    5.js start

--- leetcode1-10/5.js
+++ leetcode1-10/5.js
@@ -1,3 +1,11 @@
 var longestPalindrome = function(s) {
     var max_length = 0, maxp = '';
+
+
+    for (var i =0; i < s.length; i++){
+        var subs = s.substr(i, s.length);
+        console.log(subs)
+        console.log(typeof(subs))
+    }
 };
+longestPalindrome('abcbadea');
