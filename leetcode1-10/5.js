f92428fa33878bdf8a3aea3fe0ba3bd71a278a46
jgam <jgam@alumni.nd.edu>
2019-06-20 10:57:07 +0900

    done 5.js

--- leetcode1-10/5.js
+++ leetcode1-10/5.js
@@ -1,11 +1,28 @@
+function is_Palindrome(str1){
+    var rev = str1.split("").reverse().join("");
+    return str1 == rev;
+}
+
 var longestPalindrome = function(s) {
     var max_length = 0, maxp = '';
 
 
     for (var i =0; i < s.length; i++){
-        var subs = s.substr(i, s.length);
-        console.log(subs)
-        console.log(typeof(subs))
+        var subs = s.substr(i, s.length);//substring from i to s.lenght
+        for(var j = subs.length; j>=0; j--){
+            var sub_subs_str = subs.substr(0, j);
+            
+            if(sub_subs_str.length <= 1) continue;
+
+            if(is_Palindrome(sub_subs_str)){
+                if(sub_subs_str.length > max_length){
+                    max_length = sub_subs_str.length;
+                    maxp = sub_subs_str;
+                }
+            }
+        }
     }
+    console.log(maxp);
+    return maxp;
 };
 longestPalindrome('abcbadea');
