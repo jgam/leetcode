c6956cc313b2b9021722e35dea5106e051ec25e9
jgam <jgam@alumni.nd.edu>
2019-06-24 16:37:55 +0900

    initial for loops BUT linear loop

--- leetcode1-10/6.js
+++ leetcode1-10/6.js
@@ -1,4 +1,18 @@
 var convert = function(s, numRows) {
+    //num rows
+    let vertical = (numRows-2)*2+2
+    var arr = Array(numRows).join(".").split(".");
+
+    console.log(arr)
+    for(let i = 0; i<s.length; i+=vertical){
+        for(let j = vertical-1; j>i; j--){//j>i needs to be defined
+
+        }
+        s[i]
+    }
+    //outer for loop of num Rows and each for loop inside should have 
+    //for loop through vertical number and att it to the return string
+    //after the first loop, 
     return s;
 };
 
