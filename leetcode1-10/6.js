526db154d266b8c6720cbc2493b09b5b4d461e6c
jgam <jgam@alumni.nd.edu>
2019-06-25 12:22:16 +0900

    done 6.js

--- leetcode1-10/6.js
+++ leetcode1-10/6.js
@@ -1,74 +1,72 @@
 var convert = function(s, numRows) {
     //num rows
 
+    //exception of one row
     if(numRows==1){
         return s;
     }
 
+    //getting loop addition variables
     let vertical = (numRows-2)*2+2
     var arr = Array(numRows).join(".").split(".");
     let start = 0;
 
-    //console.log(arr)
-
+    //for loop for starting the first or top row loop
     for(let i = 0; i<s.length; i+=vertical){
         start = i
         let arr_index = 0;
         arr[arr_index] += s[i]
         
-        for(let j = vertical-1; j>0; j--){//j>i needs to be defined
+        //starting the remaining arr_index rows loop
+        for(let j = vertical-1; j>0; j--){//the looping range isn't important because it will break when arr_index == numRows
             arr_index += 1;
             start+=1;
+
+            //check the break
             if(arr_index == numRows){
                 break;
             }
             
+            //check the break (not important, just in case)
             if(start > s.length){
                 break;
             }
+
+            //the last row needs to be added
             if (start == j){
                 arr[arr_index] = s[start];
                 break;
             }
+
             //try and except for the last loop - kinda long but whatever
             try{
+                //first undefined then don't add
                 if(s[start]==undefined){
                     continue
                 }
-                /*
-                console.log('here is b?')
-                console.log(s[start])
-                console.log(s[start+j-arr_index])
-                console.log(start, j)
-                console.log(start+j, s.length);//what if there is undefined, then don't add to the string
-                */
-                arr[arr_index]+=s[start];//this needs to be fixed to pass the test
-                //this needs to be fixed
+                
+                //adding
+                arr[arr_index]+=s[start];
+                
+                //arr_index not to exceed the arr_index
                 if(arr_index == numRows-1){
                     continue
                 }
-                /*
-                if(start+j <= s.length){//changed to <= from <
-                    arr[arr_index] += s[start+j-arr_index];
-                }
-                */
-               if(s[start+j-arr_index] == undefined){
+                
+                //important check of whether the second letter should be added or not
+                if(s[start+j-arr_index] == undefined){
                    continue
-               }
-               else{
+                }
+                else{
                    arr[arr_index] += s[start+j-arr_index];
-               }
+                }
             }
             finally{
                 continue;
             }
-            //this needs to be done
         }
-        //console.log(arr);
     }
-    //outer for loop of num Rows and each for loop inside should have 
-    //for loop through vertical number and att it to the return string
-    //after the first loop, 
+    //joining the array into a string
     return arr.join('');
 };
 
