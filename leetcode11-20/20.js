42cc7dc2142f7927898aa85fda3cadd179e1f673
jgam <jgam@alumni.nd.edu>
2019-06-27 10:11:25 +0900

    first draft: logic error

--- leetcode11-20/20.js
+++ leetcode11-20/20.js
@@ -3,5 +3,40 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    var myStack = [];
+    let open = '([{';
+    let close = {']':'[', '}':'{', ')':'('};
+
+    for(let i = 0; i<s.length; i++){
+        if(open.includes(s[i])){
+            myStack.push(s[i]);
+        }
+        else{
+            if(myStack[myStack.length-1] === close[s[i]]){
+                myStack.pop(myStack.length-1);
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    if(myStack===[]){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+*/
+
+console.log(isValid('()[]{}'));
