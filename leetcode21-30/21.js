a12678e62c27d269e66887b55f3f841617be5fe4
jgam <jgam@alumni.nd.edu>
2019-06-28 18:22:47 +0900

    done

--- leetcode21-30/21.js
+++ leetcode21-30/21.js
@@ -11,14 +11,25 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if(l1.val === null & l2.val ===null){
-        return null;
-    }
-    else{
-        if(l1.val > l2.val){
-            
+    var res = new ListNode(-1);
+    var temp = res;
+    while(l1 && l2) {
+        if (l1.val < l2.val) {
+            res.next = l1;
+            l1 = l1.next;
+        } else {
+            res.next = l2;
+            l2 = l2.next;
         }
+        res = res.next;
     }
+    if (l1) {
+        res.next = l1;
+    }
+    if (l2) {
+        res.next = l2;
+    }
+    return temp.next;
 };
 
 
