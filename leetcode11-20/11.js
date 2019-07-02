3c8d504c229201c63a8fc4c54f7b34fb0a6b6bba
jgam <jgam@alumni.nd.edu>
2019-07-02 15:12:20 +0900

    done

--- leetcode11-20/11.js
+++ leetcode11-20/11.js
@@ -8,96 +8,22 @@ Output: 49
 
 */
 var maxArea = function(height) {
-    //height is the list
-    //we need to start from the middle.
-    //decide which direction to move (right or left) based on comparing the number(small number direction)
-    //now what if the two containers have same height
-
-    //micro level architecting
-    //first start with defining start and end index
-    var middle = parseInt(height.length/2);
-    var start = middle-1;
-    var end = middle;
+    var start = 0;
+    var end = height.length-1;
     let max_area = 0;
-    let current_area = 0;
+    while(start != end ){
+        max_area = Math.max(max_area, Math.min(height[start], height[end]) * (end-start));
 
-    //start the for loop and keep expanding the array
-    /*
-    function comparing(current_area, start, end, height){
-        let max_area2 = current_area;
-        let start2 = start;
-        let end2 = end;
-        let height2 = height;
-        let current_area2;
-
-        while(start2 ===0 && end2===height2.length-1){
-            current_area2 = Math.min(heihgt2[start2], height2[end2]) * (end2-start2);
-            
-            //comparing current and max area
-            if(current_area > max_area){
-                max_area2 = current_area;
-            }
-
-            //now adding 1 to start or end
-            if(start2===0){
-                end2 += 1;
-            }
-            else if(end2===height2.length-1){
-                start2 -= 1;
-            }
-            else if(height2[start]>height2[end]){
-                end2 += 1;
-            }
-            else if(height2[start]<height2[end]){
-                start2 -= 1;
-            }
-            else{
-                //so here is when start and end are the same...in here we should 
-                // we have to go two ways to make sure check everything
-                start2 -= 1;
-                continue;
-
-
-                //return Math.min(start_subtracting, end_addition);
-            }
-        }
-        return max_area2;
-
-    }
-    */
-
-    while(start===0 && end ===height.length-1){
-        
-        current_area = Math.min(heihgt[start], height[end]) * (end-start);
-        
-        //comparing current and max area
-        if(current_area > max_area){
-            max_area = current_area;
-        }
-
-        //now adding 1 to start or end
-        if(start===0){
-            end += 1;
-        }
-        else if(end===height.length-1){
-            start -= 1;
-        }
-        else if(height[start]>height[end]){
-            end += 1;
-        }
-        else if(height[start]<height[end]){
-            start -= 1;
+        if(height[start]> height[end]){
+            end -= 1;
         }
         else{
-            //so here is when start and end are the same...in here we should 
-            // we have to go two ways to make sure check everything
+            start += 1;
         }
-
-
     }
     return max_area;
 
 
 };
 
-console.log(maxArea([1,2,3,4,5,6,7,8,9]))
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
