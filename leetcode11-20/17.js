c4eb8bd6c4804851bfd7705914a6a99df0301747
jgam <jgam@alumni.nd.edu>
2019-07-04 09:32:26 +0900

    start

---
+++ leetcode11-20/17.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    let bruh = {"1": '', "2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz", "0":" "};
+
+    
+};
