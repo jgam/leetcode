4125f617114f42399c398a23ffe3ebfda489fb95
jgam <jgam@alumni.nd.edu>
2019-07-04 10:10:00 +0900

    saved it to hash map

--- leetcode11-20/17.js
+++ leetcode11-20/17.js
@@ -3,7 +3,7 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    let bruh = {"1": '', "2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz", "0":" "};
+    let bruh = {"1": '', "2":["a","b","c"], "3":["d","e","f"], "4":["g","h","i"], "5":["j","k","l"], "6":["m","n","o"], "7":["p","q","r","s"], "8":["t","u","v"], "9":["w","x","y","z"], "0":[" "]};
+    let ret = [];
 
-    
 };
