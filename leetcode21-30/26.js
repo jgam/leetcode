83b58e57113c1c7b2673b4fead1a3008843534c0
jgam <jgam@alumni.nd.edu>
2019-07-16 16:31:17 +0900

    first try

--- leetcode21-30/26.js
+++ leetcode21-30/26.js
@@ -3,7 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    var ret_list = [];
+    for(let i = 0; i < nums.length; i++){
+        if(ret_list.includes(nums[i])){
+            continue;
+        }
+        else{
+            ret_list.push(nums[i]);
+        }
+    }
+    return ret_list;
 };
 
 console.log(removeDuplicates([1,1,2]));//shoud print [1,2]
