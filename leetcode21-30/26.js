c2c9e42f5b2f7981f405eb7ac550dcf0b1d52be0
jgam <jgam@alumni.nd.edu>
2019-07-16 16:47:55 +0900

    26

--- leetcode21-30/26.js
+++ leetcode21-30/26.js
@@ -12,7 +12,17 @@ var removeDuplicates = function(nums) {
             ret_list.push(nums[i]);
         }
     }
-    return ret_list;
+    nums = ret_list;
+    return ret_list.length;
 };
 
-console.log(removeDuplicates([1,1,2]));//shoud print [1,2]
+
+let nums = [1,1,2,2];
+// nums is passed in by reference. (i.e., without making a copy)
+let len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (let v= 0; v < len; v++){
+    console.log(nums[v]);
+}
