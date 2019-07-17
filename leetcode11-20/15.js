db1e53cf63969f5f75d9cdf3d0df55bcc98253f4
jgam <jgam@alumni.nd.edu>
2019-07-17 16:53:10 +0900

    15 in progress

--- leetcode11-20/15.js
+++ leetcode11-20/15.js
@@ -2,9 +2,26 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {//numa ia the array
-    //to get a unique sum from the list
-    for(let i = 0; i < nums.length; i++){
-        
+var threeSum = function(nums) {
+    nums.sort()
+    console.log(nums)
+    var ret = [];
+    for(let i = 0; i<nums.length; i++){
+        for(let j =i+1; j< nums.length; j++){
+            let current = 0;
+            current = current - nums[i] - nums[j];
+            //the for loop range needs to be fixed here
+            if(nums.slice(j).includes(current)){
+                //this if needs to be checked
+                if(ret.includes([nums[i],nums[j],current])){
+                    continue
+                }
+                else{
+                    ret.push([nums[i],nums[j],current]);
+                }
+            }
+        }
     }
+    console.log(nums.slice(5))
+    return ret;
 };
