2df77fd06d2a3e9c6ab6595962924567315fe768
jgam <jgam@alumni.nd.edu>
2019-07-25 16:22:09 +0900

    done

--- leetcode21-30/27.js
+++ leetcode21-30/27.js
@@ -4,11 +4,21 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let count = 0
-    for (let i = 0; i < nums.length; i++){
-        if(nums[i] == val){
-            count += 1;
+    let count = 0;
+    let current = 0;
+    let subtract = 0;
+    var ret = [1,2,3];
+    
+    while(count <= nums.length){
+        count += 1;
+        if(nums[current] == val){
+            //console.log('here');
+            count -= 1;
+            nums.splice(current,1);
+        }
+        else{
+            current+=1;
         }
     }
-    return nums.length - count;
+    return nums.length;
 };
