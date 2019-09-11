866989f6f3e5bf07908b5f0960c27e4e790fcad7
jgam <jgam@alumni.nd.edu>
2019-09-11 14:41:37 +0900

    backtracking1 done

--- java_algo/permutations.java
+++ java_algo/permutations.java
@@ -10,26 +10,14 @@ public class Solution{
     static int current;
 
     public static void main(String[] args){
-        String abc = "heyy";
-        System.out.println(abc);
-        abc += "asdfasdf";
-        System.out.println(abc);
+
         String oh = "O";
         String ex = "X";
         
         Scanner scanner = new Scanner(System.in);
 		N = scanner.nextInt();
         current = 1;
-        System.out.println(N);
 
-        System.out.println(power(N));
-
-        ArrayList<ArrayList<String>> graph = new ArrayList<>();
-		for(int i = 0; i < N; i++){
-			graph.add(new ArrayList<String>());
-		}
-
-        System.out.println(graph);
         if(N == 1){
             System.out.println("O");
             System.out.println("X");
@@ -52,21 +40,28 @@ public class Solution{
     }
 
     public static long permute(int N, int current, int condition, String currentString){
-        while(N > current){
-            if(condition == 1){
-                currentString += "O";
-                current += 1;
-                permute(N, current, 1, currentString);
-                permute(N, current, 2, currentString);
+        if(condition == 1){
+            currentString += "O";
+            current += 1;
+            if(current == N){
+                System.out.println(currentString);
             }
             else{
-                currentString += 'X';
-                current += 1;
                 permute(N, current, 1, currentString);
                 permute(N, current, 2, currentString);
             }
         }
-        System.out.println(currentString);
+        else{
+            currentString += 'X';
+            current += 1;
+            if(current == N){
+                System.out.println(currentString);
+            }
+            else{
+                permute(N, current, 1, currentString);
+                permute(N, current, 2, currentString);
+            }
+        }
         return 0;
 
     }
