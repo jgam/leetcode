28c5d045dd490e88048167ac021c3eb523d9508f
jgam <jgam@alumni.nd.edu>
2019-09-11 15:23:17 +0900

    backtracking2 kaidan

---
+++ java_algo/kaidan.java
@@ -0,0 +1,16 @@
+//kaidan.java
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main{
+
+    static int N;
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+        System.out.println(N);
+    }
+}
