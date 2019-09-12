9d4bd6473829c7d5e628bc0ba37ee6b47add6e6e
jgam <jgam@alumni.nd.edu>
2019-09-12 10:45:47 +0900

    theater done

--- java_algo/movie.java
+++ java_algo/movie.java
@@ -5,14 +5,26 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Solution{
-    static int N, M;
+    static int N, M, ret;
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 		N = scanner.nextInt();
         M = scanner.nextInt();
+        ret = N;
 
-        System.out.println(N);
-        System.out.println(M);
         //input done
+
+        for(int i = 1; i < N; i ++){
+            ret = i * ret;
+        }
+        for(int i = 1; i <= N-M; i++){
+            ret = ret / i;
+        }
+
+        for(int i = 1; i <= M; i++){
+            ret = ret / i;
+        }
+
+        System.out.println(ret);
     }
 }
