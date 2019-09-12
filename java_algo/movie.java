c153b34dca910eb2118fdf0e33cfa7245f495d69
jgam <jgam@alumni.nd.edu>
2019-09-12 10:33:13 +0900

    movie theater setup

---
+++ java_algo/movie.java
@@ -0,0 +1,18 @@
+//kaidan.java
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Solution{
+    static int N, M;
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        System.out.println(N);
+        System.out.println(M);
+        //input done
+    }
+}
