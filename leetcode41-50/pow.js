6722254faecf1fe82d8cb60f8e61be22d9df02b5
Gam, Jeonghan <jeonghan.gam@rakuten.com>
2019-10-11 16:18:39 +0900

    initial easy implementation

--- leetcode41-50/pow.js
+++ leetcode41-50/pow.js
@@ -5,5 +5,17 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    
+    if(n === 0){
+        return 1;
+    }
+    else if(n > 0){
+        return Math.pow(x, n);
+    }
+    else{
+        let new_var;
+        new_var = parseFloat(n * -1);
+        return 1/Math.pow(x,new_var);
+    }
 };
+
+console.log(myPow(2,-3));
