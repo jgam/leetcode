82e58f7ba730f313b2aa253e87dbd39c1884c2b5
Gam, Jeonghan <jeonghan.gam@rakuten.com>
2019-10-15 15:36:04 +0900

    initial

--- leetcode41-50/48.js
+++ leetcode41-50/48.js
@@ -3,5 +3,23 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    
+    var n = matrix.length-1;
+    var return_array = [...Array(n+1)].map(e => Array(n+1));;
+
+    for(var i = n; i >= 0; i--){
+        for(var j =0; j < n+1; i++){
+            return_array[j][i] = matrix[i][j];
+        }
+    }
+
+    return return_array;
+
+
+
 };
+
+console.log(rotate([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ]));
