f1590d23a15d754f6374e8bd8d30c82ce3a0894d
Gam, Jeonghan <jeonghan.gam@rakuten.com>
2019-10-17 12:36:10 +0900

    done

--- leetcode41-50/48.js
+++ leetcode41-50/48.js
@@ -4,21 +4,12 @@
  */
 var rotate = function(matrix) {
     var n = matrix.length-1;
-    var return_array = [...Array(n+1)].map(e => []);
+    var return_array = [...Array(n+1)].map(e => new Array(n+1));
     
-    console.log(matrix);
-    console.log(matrix[0]);
-    console.log(typeof(matrix[0]));
-    console.log(matrix[2][0]);
-    console.log(typeof(matrix[2][0]));
     console.log(n);
-    console.log(return_array[0][2]);
-    console.log(return_array[0].push(3));
-    console.log(return_array)
-
     for(var i = n; i >= 0; i--){
-        for(var j =0; j < n+1; i++){
-            return_array[j][i].push(matrix[i][j]);
+        for(var j =0; j < n+1; j++){
+            return_array[j][i] = matrix[i][j];
         }
     }
 
