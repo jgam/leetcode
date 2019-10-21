856474770809a8da5dde5379faefaa99881c9ff0
jgam <jgam@alumni.nd.edu>
2019-10-21 12:59:38 +0900

    done

--- leetcode31-40/34.js
+++ leetcode31-40/34.js
@@ -5,5 +5,23 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    
+    var cond_num = 0;
+    var curr_index = nums.length;
+    var start = 0;
+    var end = 0;
+
+    while(true){
+        if(curr_index %2 == 0){
+            curr_index = curr_index / 2;
+        }
+        else{
+            curr_index = curr_index - 1;
+            curr_index = curr_index / 2;
+        }
+    }
+
+
 };
+
+
+console.log(searchRange([1,2,3,4,5],2));
