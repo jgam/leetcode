421e1596da3e9b931c3311c9faa14a2ea4441207
jgam <jgam@alumni.nd.edu>
2019-10-24 17:11:36 +0900

    exception

--- leetcode11-20/14.js
+++ leetcode11-20/14.js
@@ -3,23 +3,33 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+    if (strs.length === 0){
+        return '';
+    }
+    
+    if(strs.length === 1){
+        return strs[0];
+    }
+    
     strs.sort(function(a,b){
         return a.length - b.length;
     });
     var current = strs[0];
     strs.shift();
-    for(var i = current.length-1; i >= 0; i--){
+    for(var i = current.length; i >= 0; i--){
         //for loop the smallest from end to start
         var new_str = current.slice(0, i)
-        for(var j = 0; j < j.length; j++){
-            console.log(strs[j],new_str);
+        for(var j = 0; j < strs.length; j++){
+            //console.log(strs[j],new_str);
             if(strs[j].indexOf(new_str) >= 0){
+                if(j === strs.length -1){
+                    return new_str
+                }
                 continue;
             }
             else{
                 break;
             }
-            return new_str;
         }
     }
 
