7fa174e42b4e370abb4dc7df3b46519ecab29452
jgam <jgam@alumni.nd.edu>
2019-10-24 16:31:18 +0900

    set up done

--- leetcode11-20/14.js
+++ leetcode11-20/14.js
@@ -3,5 +3,11 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
+    strs.sort(function(a,b){
+        return a.length - b.length;
+    });
+
+    console.log(strs);
 };
+
+longestCommonPrefix(['flower','flo','flour']);
