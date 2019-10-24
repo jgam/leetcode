61cc226e38bc160396a1b8768d6f79a22ea53943
jgam <jgam@alumni.nd.edu>
2019-10-24 16:54:23 +0900

    initial try

--- leetcode11-20/14.js
+++ leetcode11-20/14.js
@@ -6,8 +6,24 @@ var longestCommonPrefix = function(strs) {
     strs.sort(function(a,b){
         return a.length - b.length;
     });
+    var current = strs[0];
+    strs.shift();
+    for(var i = current.length-1; i >= 0; i--){
+        //for loop the smallest from end to start
+        var new_str = current.slice(0, i)
+        for(var j = 0; j < j.length; j++){
+            console.log(strs[j],new_str);
+            if(strs[j].indexOf(new_str) >= 0){
+                continue;
+            }
+            else{
+                break;
+            }
+            return new_str;
+        }
+    }
 
-    console.log(strs);
+    return ''
 };
 
-longestCommonPrefix(['flower','flo','flour']);
+console.log(longestCommonPrefix(['flower','flo','flour']));
