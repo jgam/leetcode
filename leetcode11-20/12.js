325b0875729770bd69ccf15542d8f7be41f65d75
jgam <jgam@alumni.nd.edu>
2019-10-24 11:15:19 +0900

    new start

--- leetcode11-20/12.js
+++ leetcode11-20/12.js
@@ -6,8 +6,21 @@ var intToRoman = function(num) {
     console.log(num);
     let str2num = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
     let ret = '';
-    
-    
+
+    function getString(number){
+        let ret_string = '';
+        
+        return ret_string;
+    }
+
+    function getmoreString(number){
+        let ret_string= '';
+
+        return ret_string;
+    }
+
+    var current = 0;
+    var mod_num = 10;
     
     
 };
