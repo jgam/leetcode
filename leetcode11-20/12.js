a4433df20a50f5ede47651f487bfbc479c1d017e
jgam <jgam@alumni.nd.edu>
2019-10-24 15:27:31 +0900

    done

--- leetcode11-20/12.js
+++ leetcode11-20/12.js
@@ -7,33 +7,40 @@ var intToRoman = function(num) {
     let str2num = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
     let ret = '';
 
-    function getString(number, bracket){
+    function getString(real_number, bracket){
         //bracket is 10 to the power
         let ret_string = '';
+        let number = real_number / bracket;
         let count = number;
         let numString = bracket.toString();
 
         if(number === 4){
             //do 4
-            ret += str2num[numString]+str2num[(bracket*5).toString()];
+            ret_string = str2num[numString]+str2num[(bracket*5).toString()];
         }
         else{
             //do others
-            ret += str2num[numString].repaet(count);
+            ret_string = str2num[numString].repeat(count);
         }
-        
+
         return ret_string;
     }
 
-    function getmoreString(number, bracket){
+    function getmoreString(real_number, bracket){
+        //bracket = 100, 1000 , etc
         let ret_string= '';
+        let number = real_number / bracket;
         let count = (number - 5)
+        let numString = bracket.toString();
 
         if(number === 9){
+            let tenNum = (10 * bracket).toString();
+            ret_string = str2num[numString]+str2num[tenNum];
             //do 9
         }
         else{
             // do 10
+            ret_string = str2num[(bracket*5).toString()] + str2num[numString].repeat(count)
         }
         return ret_string;
     }
@@ -44,23 +51,30 @@ var intToRoman = function(num) {
     var current_num = num.toString();
     console.log(current_num.length);
     var current_length = current_num.length-1;
-
+    console.log('in the for loop');
     for(var i = 0; i < current_num.length; i++){
+        console.log(current_num);
         let real_number;
         let number_bracket = 10 ** current_length;
-        real_number = int(current_num[i]) * number_bracket;
+        real_number = parseInt(current_num[i]) * number_bracket;
         
-        if(int(current_num[i] > 5)){
+        if(parseInt(current_num[i] > 5)){
+            console.log('number of get more string gets called should be none');
             //run more than 5
-            getmoreString(real_number/number_bracket, number_bracket);
+            ret += getmoreString(real_number/number_bracket, number_bracket);
         }
         else{
+            console.log('here comes a lot');
+            console.log(real_number, number_bracket);
             //run less than 5
-            getString(real_number, number_bracket);
+            ret += getString(real_number, number_bracket);
         }
         
         current_length -= 1
     }
+
+    console.log(ret);
+    return null;
 };
 
 intToRoman(1234)
