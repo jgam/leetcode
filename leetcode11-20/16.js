bffef5c9f2c323128360395e2eb6d58364abbf7d
jgam <jgam@alumni.nd.edu>
2019-10-25 13:21:00 +0900

    done

--- leetcode11-20/16.js
+++ leetcode11-20/16.js
@@ -4,5 +4,11 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
+    var current_array = nums.sort(function(a, b){return a - b});
+
+    console.log(current_array);
+
     
 };
+
+console.log(fourSum([0,-1,2,1,0,-2], 0));
