0f39c65fd8fb406aebf867a8b1d1195c35e80a41
jgam <jgam@alumni.nd.edu>
2019-10-28 12:41:22 +0900

    done

--- leetcode31-40/35.js
+++ leetcode31-40/35.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    
+    var boolean_switch = false;
+
+    for(var i = 0; i < nums.length; i++){
+        if(nums[i]==target){
+            return i;
+        }
+        else if(nums[i] > target){
+            return i;
+        }
+        else{
+            continue;
+        }
+    }
+    return nums.length;
 };
+
+console.log(searchInsert([1,3,5,6],5))
