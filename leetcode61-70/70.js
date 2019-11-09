81a995b537f43e0e03b08d517f51f23241610517
jgam <jgam@alumni.nd.edu>
2019-11-09 14:35:30 +0900

    initial submit

--- leetcode61-70/70.js
+++ leetcode61-70/70.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else if (n === 2) {
+    return 2;
+  } else {
+    return climbStairs(n - 1) + climbStairs(n - 2);
+  }
 };
+
+console.log(climbStairs(2)); //output 3
