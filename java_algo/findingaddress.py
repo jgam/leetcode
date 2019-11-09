ede33154762fa08560c2dfcdd7d66b36478616e8
jgam <jgam@alumni.nd.edu>
2019-11-09 15:13:48 +0900

    done

--- java_algo/findingaddress.py
+++ java_algo/findingaddress.py
@@ -2,9 +2,8 @@ import sys
 
 read = lambda : sys.stdin.readline().strip()
 
-print(read)# this only outputs data type of read which is the lambda function
-
 n = int(read())
+
 
 def dfs(matrix, cnt, x,y):
     matrix[x][y]=0
@@ -16,7 +15,7 @@ def dfs(matrix, cnt, x,y):
         n_y = y + dy[i]
         # go to near n_x and n_y
         if n_x>=0 and n_x<n and n_y>=0 and n_y<n:
-            # region/boundary check
+            # region/boundary check in the array of n x n matrix
             if matrix[n_x][n_y]==1:
             # when that part is equal to 1
                 cnt = dfs(matrix, cnt+1, n_x, n_y)
@@ -31,7 +30,6 @@ ans = []
 for i in range(n):
     for j in range(n):
         if matrix[i][j]==1:
-            #this is something group with 1s?
             ans.append(dfs(matrix, 1, i, j))
 
 print(len(ans))
