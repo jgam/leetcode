42b8d423d141066869782ae329cf1458b7b2795a
jgam <jgam@alumni.nd.edu>
2019-12-16 09:44:21 +0900

    test

--- java_algo/diameterofTree.py
+++ java_algo/diameterofTree.py
@@ -1,19 +1,23 @@
-node = int(input())
+node = int(raw_input())
 tree_info = [[] for _ in range(node + 1)]
 
 for _ in range(node - 1):
-    parent, child, length = map(int, input().split())
+    parent, child, length = map(int, raw_input().split())
     tree_info[parent].append((child, length))
 
+print(tree_info)
 
 #BFS
 def bfs(graph_list, start):
     visited = []
     queue = [start]
-    
+    print('original queue')
     while queue:
+        print(queue)
         node = queue.pop(0)
+        print(node)
         visited.append(node)
+        print(visited)
         for child in graph_list[node]:
             queue.append(child[0])
             
@@ -23,9 +27,11 @@ def bfs(graph_list, start):
 #list creation for sorting
 bfs_node = bfs(tree_info, 1)
 max_node = [0 for _ in range(node + 1)]
+print(max_node)
 diameter = [[0] for _ in range(node + 1)]
+print(diameter)
 max_diameter = [0 for _ in range(node + 1)]
-
+print(max_diameter)
 
 
 #from the bottom of node, diameters extracted
