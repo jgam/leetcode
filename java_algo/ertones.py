054aa682254f641d3804ae72a27ec489f9db3799
jgam <jgam@alumnio.nd.edu>
2020-01-10 12:42:24 +0900

    done with ertonese

--- java_algo/ertones.py
+++ java_algo/ertones.py
@@ -11,7 +11,7 @@ first_num, second_num = the_string.split()
 
 num_list = [0] * int(second_num)
 
-print(num_list)
+#print(num_list)
 
 #now sqrt list
 
@@ -22,4 +22,12 @@ for i in range(2, m+1):
         for j in range(i+i, int(second_num), i):
             num_list[j] = 1
 num_list[1] = 1
-print([ i for i in range(int(first_num), int(second_num)) if num_list[i]==0])
+##print([ i for i in range(int(first_num), int(second_num)) if num_list[i]==0])
+
+for i in range(int(first_num), int(second_num)):
+    if num_list[i]==0:
+        print(i)
+    else:
+        continue
+
+
