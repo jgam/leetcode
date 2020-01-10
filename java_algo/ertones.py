79ffe3888a133682075750d884ca8a8321c04756
jgam <jgam@alumnio.nd.edu>
2020-01-10 12:29:39 +0900

    ertones done

--- java_algo/ertones.py
+++ java_algo/ertones.py
@@ -9,3 +9,20 @@ print(second_num)
 #end num
 
 #begin num
+
+#create two lists: 1. sqrt for checking duplicatees, 2. true with the list of booleans
+
+num_list = [0] * int(second_num)
+
+print(num_list)
+
+#now sqrt list
+
+m = int(int(second_num) ** 0.5)
+
+for i in range(2, m+1):
+    if num_list[i] == 0:
+        for j in range(i+i, int(second_num), i):
+            num_list[j] = 1
+num_list[1] = 1
+print [i for i in range(int(first_num), int(second_num)) if num_list[i]==0]
