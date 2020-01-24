365c4c5be9c30f72b45b76fe8ecab03c76517b34
jgam <jgam@alumnio.nd.edu>
2020-01-24 15:35:06 +0900

    string3 done

--- kakao/string3.py
+++ kakao/string3.py
@@ -14,6 +14,8 @@ def isValid(s):
         return 'NO'
 
     '''
+
+    '''
     count_list = []
     currentAlph = s[0]
     currentCount = 1
@@ -41,3 +43,9 @@ def isValid(s):
     if (max(count_list) - min(count_list)) > 1:
         return 'NO'
     return 'YES'
+'''
+s = input().strip()
+freq = collections.Counter(s)#{'a':2, 'b':2...'d':1}
+values = list(freq.values())#[2,2,1,1]
+values.sort()#[1,1,2,2]
+print('YES' if values.count(values[0]) == len(values) or (values.count(values[0]) == len(values) - 1 and values[-1] - values[-2] == 1) or (values.count(values[-1]) == len(values) - 1 and values[0] == 1) else 'NO')
