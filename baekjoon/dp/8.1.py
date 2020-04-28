5b16b4ab4482b423bc59603c0f1b4b91a57fa0f2
jgam <jgam@alumni.nd.edu>
2020-04-28 15:05:07 +0900

    refactoring

--- baekjoon/dp/8.1.py
+++ baekjoon/dp/8.1.py
@@ -5,9 +5,10 @@ n = int(input())
 dp = [0,1,2,4,7]
 
 if n < 5:
-    return dp[n]
+    print(dp[n])
+    print('the program ended')
 
 for i in (5,n):
     dp.append(dp[i-3]+dp[i-2]+dp[i-1])
 
-return dp[-1]
+print(dp[-1])
