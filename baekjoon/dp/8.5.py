63aa41721a98ba30c07cd7593267bf1aa62322dd
jgam <jgam@alumni.nd.edu>
2020-04-29 14:17:43 +0900

    strings with repeat

---
+++ baekjoon/dp/8.5.py
@@ -0,0 +1,11 @@
+#hanoi tower
+
+#given n number of tower heights, find 
+
+#done
+
+#8.6 the strings with no repeats
+
+def solution(nums):
+    #create dp and store all the sequence
+    dp = [[]]
