495ce30cd375099c7b749321a04e717af06e0a90
jgam <jgam@alumni.nd.edu>
2021-07-01 10:06:18 +0900

    Create go8.go

---
+++ golang/go8.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+import(
+    "fmt"
+)
+
+func getDepth(curRoot *TreeNode, curDepth int) int {
+    leftDepth := curDepth
+    rightDepth:= curDepth
+    if curRoot.Left != nil{
+        leftDepth = getDepth(curRoot.Left, curDepth + 1)
+    }
+    
+    if curRoot.Right != nil{
+        rightDepth = getDepth(curRoot.Right, curDepth + 1)
+    }
+    if leftDepth > rightDepth {
+        return leftDepth
+    } else {
+        return rightDepth
+    }
+}
+
+func maxDepth(root *TreeNode) int {
+    if root ==nil{
+        return 0
+    }
+    return getDepth(root, 1)
+}
