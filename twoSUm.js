452b038f8383809bd5fda6f7352cc02e80dd6dd6
jgam <jgam@alumni.nd.edu>
2021-12-07 22:18:32 +0900

    Create twoSUm.js

---
+++ twoSUm.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    const newObj = {}
+    nums.forEach((num, i) => newObj[num] = i)
+    
+    for(let i in nums){
+        if(target-nums[i] in newObj){
+            console.log(typeof i, typeof newObj[target-nums[i]])
+            if(parseInt(i) !== newObj[target-nums[i]]) {return [i, newObj[target-nums[i]]]}
+        }
+    }
+    return -1
+};
