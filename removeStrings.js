9d16b49345fbb6444b6a3e1901315bdfabbd5763
jgam <jgam@alumni.nd.edu>
2022-05-06 14:48:29 +0900

    Create removeStrings.js

---
+++ removeStrings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var removeDuplicates = function(s, k) {
+    const stack = [];
+    
+    for(let char of s) {
+        if(stack.length && stack[stack.length-1][0] === char) {
+            stack[stack.length-1][1] += 1;
+            if(stack[stack.length-1][1] === k) {
+                stack.pop();
+            }
+        } else {
+            stack.push([char, 1]);
+        }
+    }
+    
+    let res = '';
+    
+    for(let [char, count] of stack) {
+        res += char.repeat(count);
+    }
+    
+    return res;
+};
